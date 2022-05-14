377ba8c26efb7a90dc26bbe10bb3297829a6c2fc
~/alpha <s201830040@kfupm.edu.sa>
2022-05-14 10:55:35 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# ICS 343 Quizzes Solver
+
+## How to use 🤔?
+
+Go to any quiz in the website, e.g., quiz 18 and its URL is
+
+```md
+// quiz 18
+https://highered.mheducation.com/sites/0073376221/student_view0/chapter18/quizzes.html
+```
+
+then press F12 and copy & paste after you change the line below with <YourFullNameName> & <YourIDNumber>
+
+```javascript
+getFullMark(getQuestionObjectArray(), 'YourFullNameName', 'YourIDNumber');
+```
+
+## POC
+
+![alt text](assets\img1.png)
---
+++ script.js
@@ -0,0 +1,23 @@
+const getFullMark = (arr, name, id) => {
+  const buttons = document.querySelectorAll('input[type=radio]');
+  const txtName = document.getElementById('txtName');
+  const sectionId = document.getElementById('sectionid');
+  txtName.value = name;
+  sectionId.value = id;
+  const correctAnswers = [];
+  arr.forEach((question) => {
+    question.answerObjectsArray.forEach((feedback) => {
+      correctAnswers.push(feedback.feedBackTitle);
+    });
+  });
+  i = 0;
+  buttons.forEach((button) => {
+    if (correctAnswers[i] === 'Õùääóõâ¶×øåáóä150') {
+      button.checked = true;
+    }
+    i++;
+  });
+  document.querySelector('input[value="Submit Answers"]').click();
+};
+// fill <YourFullNameName> & <YourIDNumber>
+getFullMark(getQuestionObjectArray(), 'YourFullNameName', 'YourIDNumber');
